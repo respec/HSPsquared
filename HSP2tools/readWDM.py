d6371eec88378d9a5e58414b723b173131883d89
Paul Tomasula <31142705+ptomasula@users.noreply.github.com>
2021-03-02 12:21:11 -0500

    remove deprecated functions
    The rewrite to process wdm files as groups led to the deprecation of the getfloats and adjustNval functions. Additionally Hua refactored the original process_groups method in a previous commit as process_groups2. The original process_groups method was removed and process_groups2 was renamed to process_group.

--- HSP2tools/readWDM.py
+++ HSP2tools/readWDM.py
@@ -137,8 +137,7 @@ def readWDM(wdmfile, hdffile, jupyterlab=True):
                 #findex = getfloats(iarray, farray, floats, findex, rec, offset, count, finalindex, tcode, tstep)
             
             #replaced with group/block processing approach
-            #dates, values = process_groups(iarray, farray, groups, finalindex)
-            dates, values = process_groups2(iarray, farray, groups, tgroup)
+            dates, values = process_groups(iarray, farray, groups, tgroup)
 
             ## Write to HDF5 file
             series = pd.Series(values, index=dates)
@@ -200,94 +199,6 @@ def leap_year(y):
     else:
         return False
 
-def process_groups(iarray, farray, groups, finalindex, array_chunk_size=100000000):
-        
-    record, offset = groups[0]
-
-    group_indices = [r * 512 + o for r, o in groups]
-    pscbkr = iarray[record * 512 + 2] #should always be 0 for first record in timeseries
-    pscfwr = iarray[record * 512 + 3] #should be 0 for last record in timeseries    
-
-
-    #preallocating an array to write values to should have faster read times because it prevent extent the array
-    #each time we read a block, however with irregular timesteps there is no way to know exact how big the array 
-    #to hold the entire timeseries until you read the whole timeseries. So the solution I came up with was to 
-    #preallocate chuck and then check concatanate a new chunk if the array + new data will overflow the current array.
-    #default chunk size = 1MB
-    array_index = 0
-    date_array =  np.zeros(array_chunk_size, dtype='M8[us]')
-    value_array = np.zeros(array_chunk_size, dtype=np.float32)
-
-    index = record * 512 + offset
-    while index < finalindex:
-        
-        #If end of record we need to move to next in chain. End of record is index 511 (512 in fortran)
-        #TODO - PRT need to confirm this with Jack but I notice at least one WDM file where the a compressed block at 510-511 
-        #was then followed by another 32bit value that was not a block control word at 512. 
-        ##Perhaps there is sometype of flag in indicate we cannot fit another block so go to the next one.
-        if index % 512 == 0 or (index + 1) % 512 == 0:
-            index = (pscfwr - 1) * 512 + 4
-            record = pscfwr
-            pscbkr = iarray[(record - 1) * 512 + 2] #should always be 0 for first record in timeseries
-            pscfwr = iarray[(record - 1) * 512 + 3] #should be 0 for last record in timeseries
-        #check if index is start of new group
-        elif index in group_indices:
-            current_date = splitdate(iarray[index])
-            index += 1
-        #process record
-        else:
-            #read block control word
-            x = iarray[index]  # block control word or maybe date word at start of group
-            nval = x >> 16
-            ltstep = int(x >> 10 & 0x3f) #relative_delta doesn't handle numpy.32bitInt correctly so convert to python
-            ltcode = int(x >> 7 & 0x7)
-            comp = x >> 5 & 0x3
-            qual  = x & 0x1f
-            
-            #check if next block will exceed array size and allocate additional chunk if needed 
-            if nval + array_index >= value_array.shape[0]:
-                date_array = np.concatenate((date_array, np.zeros(array_chunk_size, dtype='M8[us]')))
-                value_array = np.concatenate((value_array, np.zeros(array_chunk_size, dtype=np.float32)))
-
-            #compressed - only has single value which applies to full range
-            if comp == 1:
-                #TODO -  verfiy we do not need support for code 7 or 100YRS? this is in freq but not the programmers documentation
-                for i in range(0, nval, 1):
-                    current_date = current_date + relativedelta(
-                        years= ltstep if ltcode == 6 else 0, #if need to support 100yrs modify this line
-                        month= ltstep if ltcode == 5 else 0,
-                        days= ltstep if ltcode == 4 else 0,
-                        hours= ltstep if ltcode == 3 else 0,
-                        minutes= ltstep if ltcode == 2 else 0,
-                        seconds= ltstep if ltcode == 1 else 0
-                        )
-                    date_array[array_index + i] = current_date
-                    value_array[array_index + i] = farray[index + 1]
-                array_index += nval
-                index += 2
-            #not compressed - read nval from floats (number of values)
-            else:
-                for i in range(0, nval, 1):
-                    current_date = current_date + relativedelta(
-                        years= ltstep if ltcode == 6 else 0, #if need to support 100yrs modify this line
-                        month= ltstep if ltcode == 5 else 0,
-                        days= ltstep if ltcode == 4 else 0,
-                        hours= ltstep if ltcode == 3 else 0,
-                        minutes= ltstep if ltcode == 2 else 0,
-                        seconds= ltstep if ltcode == 1 else 0
-                        )
-                    date_array[array_index + i] = current_date
-                    value_array[array_index + i] = farray[index + 1 + i]
-                array_index += nval
-                index += 1 + nval
-    
-    date_array = date_array[0:array_index]
-    value_array = value_array[0:array_index]
-                
-    #df = pd.DataFrame({'date':date_array, 'values':value_array})
-    #df.to_csv(R'C:\users\ptomasula\desktop\debugging.csv')
-    return date_array, value_array
-
 def deltaTime(ltstep, ltcode):
     deltaT = relativedelta(
         years= ltstep if ltcode == 6 else 0, #if need to support 100yrs modify this line
@@ -301,7 +212,7 @@ def deltaTime(ltstep, ltcode):
 # HTAO - an alternative implementation of process_group:
 # 1. used lists to replace numpy matrix;
 # 2. added a loop to iterate each group and used ending date as the ending condition
-def process_groups2(iarray, farray, groups, tgroup):
+def process_groups(iarray, farray, groups, tgroup):
 
     date_array = []
     value_array = []
@@ -357,100 +268,3 @@ def process_groups2(iarray, farray, groups, tgroup):
     #df = pd.DataFrame({'date':date_array, 'values':value_array})
     #df.to_csv(R'C:\users\ptomasula\desktop\debugging.csv')
     return date_array, value_array
-
-
-def getfloats(iarray, farray, floats, findex, rec, offset, count, finalindex, tcode, tstep):
-    index = rec * 512 + offset + 1
-    stop = (rec + 1) * 512
-    cntr = 0
-    while cntr < count and findex < len(floats):
-        if index == stop -1 :
-            print ('Problem?',str(rec)) #perhaps not, block cannot start at word 512 of a record because not spot for values
-        if index >= stop-1:
-            rec = iarray[rec * 512 + 3] - 1  # 3 is forward data pointer, -1 is python indexing
-            print ('Process record ',str(rec))
-            index = rec * 512 + 4  # 4 is index of start of new data
-            stop = (rec + 1) * 512
-
-        x = iarray[index]  # block control word or maybe date word at start of group
-        nval = x >> 16
-        ltstep = x >> 10 & 0x3f
-        ltcode = x >> 7 & 0x7
-        comp = x >> 5 & 0x3
-        qual  = x & 0x1f
-        ldate = todatetime() # dummy
-        if ltstep != tstep or ltcode != tcode:
-            nval = adjustNval(ldate, ltstep, tstep, ltcode, tcode, comp, nval)
-            if nval == -1:  #unable to convert block
-                try:
-                    ldate = splitdate(x)
-                    if isinstance(ldate,datetime.date):
-                       print('Problem resolved - date found ',ldate)
-                       nval = 1
-                    else:
-                        print('BlockConversionFailure at ', str(rec + 1), ' ', str(index % 512))
-                except:
-                  print ('BlockConversionFailure at ',str(rec+ 1),' ',str(index % 512))
-                # try next word
-                comp = -1
-        index += 1
-        if comp == 0:
-            for k in range(nval):
-                if findex >= len(floats):
-                    return findex
-                floats[findex] = farray[index + k]
-                findex += 1
-            index += nval
-        elif comp > 0:
-            for k in range(nval):
-                if findex >= len(floats):
-                    return findex
-                floats[findex] = farray[index]
-                findex += 1
-            index += 1
-        cntr += nval
-    return findex
-
-def adjustNval(ldate, ltstep, tstep, ltcode, tcode, comp, nval):
-    lnval = nval
-    if comp != 1:
-        nval = -1  # only can adjust compressed data
-    else:
-        if tcode == 2:  # minutes
-            if ltcode == 6:  # from years
-                if leap_year(ldate.year):
-                    ldays = 366
-                else:
-                    ldays = 365
-                nval = ldays * lnval * 60 /ltstep
-            elif ltcode == 5: # from months
-                from calendar import monthrange
-                ldateRange = monthrange(ldate.year, ldate.month)
-                print ('month block ', ldateRange)
-                nval = ldateRange[2] * lnval * 60/ ltstep
-            elif ltcode == 4:  # from days
-                nval = lnval * 60 / ltstep
-            elif ltcode == 3:  # from hours
-                nval = lnval * 1440 / ltstep
-            else:  # dont know how to convert
-                nval = -1
-        elif tcode == 3:  # hours
-            if ltcode == 6:  # from years
-                nval = -1
-            elif ltcode == 5: # from months
-                nval = -1
-            elif ltcode == 4:  # from days
-                nval = lnval * 24 / ltstep
-            else:  # dont know how to convert
-                nval = -1
-        else:
-            nval = -1  # dont know how to convert
-
-    nval = int(nval)
-    if nval == -1:  # conversion problem
-        print('Conversion problem (tcode ', str(tcode), ', ', str(ltcode), '), (tstep ', str(tstep), ',', str(ltstep), '), (comp ', str(comp), ')')
-    else:
-        print('Conversion complete (tcode ', str(tcode), ', ', str(ltcode), '), (tstep ', str(tstep), ',', str(ltstep), '), (nval ',
-              str(nval) + ',', str(lnval), ')')
-
-    return nval
