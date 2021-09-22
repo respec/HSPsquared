90a49cab444da0b37c8786896aa26383f8ba9d21
Paul Tomasula <31142705+ptomasula@users.noreply.github.com>
2021-09-22 13:56:24 -0400

    Make compare_cases class initialization parameter

--- tests/convert/conversion_test.py
+++ tests/convert/conversion_test.py
@@ -1,10 +1,9 @@
-from tests.convert.regression_base import RegressTestBase
-
-
-class ConversionRegressionTest(RegressTestBase):
-    def __init__(self):
-        super(ConversionRegressionTest, self).__init__()
+from tests.convert.regression_base import RegressTest
 
 if __name__ == '__main__':
-    my_test = ConversionRegressionTest()
+    #compare_cases = ['test05', 'test09', 'test10', 'test10b', 'Calleg', 'GRW_Plaster', 'ZRW_WestIndian']
+    #compare_cases = ['test10']  # control test cases for comparison
+    compare_cases = ['GLWACSO']  
+
+    my_test = RegressTest(compare_cases)
     my_test.run_test()
