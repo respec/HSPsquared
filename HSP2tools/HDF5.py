68be3c7dd51396a05a3382071739ee7e0ee7016b
Paul Tomasula <31142705+ptomasula@users.noreply.github.com>
2021-09-27 21:22:16 -0400

    performance overhaul
    Replaces regression_base run_one_test method with methods that single responsibility
    Introduces multithreading support

--- HSP2tools/HDF5.py
+++ HSP2tools/HDF5.py
@@ -1,12 +1,20 @@
+from weakref import finalize
+
+from pandas.core.frame import DataFrame
 import HSP2tools
 import pandas as pd
 import os
-from typing import Union, Dict, Tuple
+from typing import Union, Dict, Tuple, final
+
+#turns out that HDF5 is not threading safe, need to implement manual locking
+from threading import Lock
 
 class HDF5:
     def __init__(self, file_name:str) -> None:
         self.file_name = file_name
         self.aliases = self._read_aliases_csv()
+        self.data = {}
+        self.lock = Lock()
 
     def _read_aliases_csv(self) -> Dict[Tuple[str,str,str],str]:
         datapath = os.path.join(HSP2tools.__path__[0], 'data', 'HBNAliases.csv')
@@ -21,20 +29,36 @@ class HDF5:
         constituent = constituent.upper()
         activity = activity.upper()
 
-        #We still need a special case for RCHES/GQUAL, IMPLAND/IQUAL and PERLAND/PQUAL
+        constituent_prefix = ''
 
+        #We still need a special case for RCHES/GQUAL, IMPLAND/IQUAL and PERLAND/PQUAL
+        if activity == 'GQUAL': 
+            constituent_prefix = 'GQUAL1_'
+
+        key = (operation,id,activity)
+        try:
+            self.lock.acquire()
+            if key not in self.data.keys():
+                self.data[key] = self._read_table(operation, id, activity)
+            self.lock.release()
+
+            df = self.data[key]
+            if constituent_prefix + constituent in df.columns:
+                return df[constituent_prefix + constituent]
+            else:
+                constituent_alias = self.aliases[(operation,activity,constituent)]
+                return df[constituent_prefix + constituent_alias]
+        except KeyError:
+            return None
+
+    def _read_table(self, operation:str, id:str, activity:str) -> pd.DataFrame:
         key = f'RESULTS/{operation}_{operation[0]}{id}/{activity}/table'
+        
         try:
             with pd.HDFStore(self.file_name, 'r') as store:
                 df = pd.read_hdf(store, key=key)
                 df['index'] = pd.to_datetime(df['index'], unit='ns')
                 df = df.set_index('index')
-            
-            if constituent in df.columns:
-                return df[constituent]
-            else:
-                constituent_alias = self.aliases[(operation,activity,constituent)]
-                return df[constituent_alias]
+                return df
         except KeyError:
-            #Meaning pathway to table or the requested constituent is not in the HDF5 file
-            return None
+            return pd.DataFrame()
--- tests/convert/regression_base.py
+++ tests/convert/regression_base.py
@@ -1,3 +1,5 @@
+from concurrent import futures
+import concurrent
 import os
 import inspect
 import webbrowser
@@ -7,24 +9,20 @@ from HSP2tools.HDF5 import HDF5
 import pandas as pd
 import numpy as np
 
-from typing import List, Tuple
+from typing import Dict, List, Tuple
+
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from copy import deepcopy
 
 
 class RegressTest(object):
-    def __init__(self, compare_cases:List[str]) -> None:
-        self.compare_cases = compare_cases
-        self.test_dir = ''
+    def __init__(self, compare_case:str) -> None:
+        self.compare_case = compare_case
         self.test_meta = {}
         self.compare_tcodes = [2]  # control time unit for comparison
+        self.compare_ids = [434]
 
-    def run_one_test_wdm(self, testdir):
-        attributes = {'location': 'unk', 'dsn': 39, 'constituent': 'PREC'}
-        filename = ''
-        df = get_wdm_data_set(filename, attributes)
-        x = []
-        y = []
-
-    def get_hbn_data(self, test_dir: str) -> List[HBNOutput]:
+    def _get_hbn_data(self, test_dir: str) -> None:
         sub_dirs = os.listdir(test_dir)
         hbn_files = []
         for sub_dir in sub_dirs:
@@ -32,23 +30,12 @@ class RegressTest(object):
                 files = os.listdir(os.path.join(test_dir, sub_dir))
                 for file in files:
                     if file.lower().endswith('.hbn'):
-                        hbn_files.append(os.path.join(test_dir, sub_dir, file))
-                break
+                        self.hspf_data = HBNOutput(os.path.join(test_dir, sub_dir, file))
+                        break 
 
-        if len(hbn_files) == 0:
-            return []
+        self.hspf_data.read_data()
 
-        hbn_data = []
-        for hbn_file_name in hbn_files:
-            hbn_output = HBNOutput(hbn_file_name)
-            hbn_output.read_data()
-            # tser = hbn_output.get_time_series('SOQUALCOD', 'hourly')
-            # HBNOutput.save_time_series_to_file(os.path.join(os.path.split(hbn_output.file_name)[0], 'zhbn.txt'), tser)
-            hbn_data.append(hbn_output)
-
-        return hbn_data
-
-    def get_hdf5_data(self, test_dir: str) -> List[HDF5]:
+    def _get_hdf5_data(self, test_dir: str) -> List[HDF5]:
         sub_dirs = os.listdir(test_dir)
         h5_files = []
         for sub_dir in sub_dirs:
@@ -56,74 +43,101 @@ class RegressTest(object):
                 files = os.listdir(os.path.join(test_dir, sub_dir))
                 for file in files:
                     if file.lower().endswith('.h5') or file.lower().endswith('.hdf'):
-                        h5_files.append(os.path.join(test_dir, sub_dir, file))
-                break
+                        self.hsp2_data = HDF5(os.path.join(test_dir, sub_dir, file))
+                        break
 
-        if len(h5_files) == 0:
-            return []
-
-        h5_data = []
-        for h5_file_name in h5_files:
-            h5_data.append(HDF5(h5_file_name))
-            
-        return h5_data
-
-    def run_one_test(self, test_dir):
-        # after getting the outputs from both hbn and hdf5
-        #   compare them one at a time
-        #   output comparison result in HTML format
-
-        hbn_data = self.get_hbn_data(test_dir)
-        hdf5_data = self.get_hdf5_data(test_dir)
-
-        results_dict = {}
-
+    def make_html_report(self, results_dict):
+        """populates html table"""
         style_th = 'style="text-align:left"'
         style_header = 'style="border:1px solid; background-color:#EEEEEE"'
-        html = '<table style="border:1px solid">\n'
-        for hbn_dataset in hbn_data:
-            for key in hbn_dataset.output_dictionary.keys():
-                html += f'<tr><th colspan=5 {style_header}>{key}</th></tr>\n'
-                html += f'<tr><th></th><th {style_th}>Constituent</th><th {style_th}>Max Diff</th><th>Match</th><th>Note</th></tr>\n'
-                (operation, activity, opn_id, tcode) = key.split('_')
-                for cons in hbn_dataset.output_dictionary[key]:
-                    print(f'    {operation}_{opn_id}  {activity}  {cons}')
-                    
-                    hbn_time_series = hbn_dataset.get_time_series(operation, int(opn_id), cons, activity, 'hourly')
-                    h5_time_series = hdf5_data[0].get_time_series(operation, opn_id, cons, activity)
+        
+        html = f'<table style="border:1px solid">\n'
 
-                    missing_data_h5 = ''
-                    missing_data_hbn = ''
-                    if hbn_time_series is None:
-                        missing_data_hbn = f'not in hbn'
-                    if h5_time_series is None:
-                        missing_data_h5 = f'not in h5'
-                    
-                    if len(missing_data_h5) > 0 or len(missing_data_hbn) > 0:
-                        html += f'<tr><td>-</td><td>{cons}</td><td>NA</td><td>NA</td><td>{missing_data_h5}<br>{missing_data_hbn}</td></tr>\n'
-                    else:
-                        #Special case, for some parameters (e.g PLANK.BENAL1) HSPF results look to be array.
-                        #Working assumption is that only the first index of that array are the values of interest.
-                        if len(hbn_time_series.shape) > 1:
-                            hbn_time_series = hbn_time_series.iloc[:,0]
-                       
-                        tolerance = 1e-2
-                        # if heat related term, compute special tolerance
-                        if cons == 'IHEAT' or cons == 'ROHEAT' or cons.startswith('OHEAT') or \
-                            cons == 'QSOLAR' or cons == 'QLONGW' or cons == 'QEVAP' or \
-                            cons == 'QCON' or cons == 'QPREC' or cons == 'QBED':
-                            abstol = max(abs(h5_time_series.values.min()), abs(h5_time_series.values.max())) * 1e-4
-                        elif cons == 'QTOTAL' or cons == 'HTEXCH' :
-                            abstol = max(abs(h5_time_series.values.min()), abs(h5_time_series.values.max())) * 1e-3    
-
-                        h5_time_series, hbn_time_series = self.validate_time_series(h5_time_series, hbn_time_series,
-                            hdf5_data[0], hbn_dataset, operation, activity, opn_id, cons)
-                            
-                        html = self.compare_time_series(h5_time_series, hbn_time_series, html, tolerance, cons)
+        for key in self.hspf_data.output_dictionary.keys():
+            operation, activity, opn_id, tcode = key.split('_')
+            if int(tcode) not in self.compare_tcodes or int(opn_id) not in self.compare_ids:
+                continue 
+            html += f'<tr><th colspan=5 {style_header}>{key}</th></tr>\n'
+            html += f'<tr><th></th><th {style_th}>Constituent</th><th {style_th}>Max Diff</th><th>Match</th><th>Note</th></tr>\n'
+            for cons in self.hspf_data.output_dictionary[key]:
+                result = results_dict[(operation,activity,opn_id, cons, tcode)]
+                no_data_hsp2, no_data_hspf, match, diff = result
+                html += self.make_html_comp_row(cons, no_data_hsp2, no_data_hspf, match, diff)
 
         html += f'</table>\n'
         return html
 
+    def make_html_comp_row(self, con:str, no_data_hsp2:bool, 
+            no_data_hspf:bool, match:bool, diff:float) -> str:
+        """populates each constituents rows"""
+        if no_data_hsp2 or no_data_hspf:
+            html = f'<tr><td>-</td><td>{con}</td><td>NA</td><td>NA</td><td>'
+            html += f'{no_data_hsp2}<br>'
+            html += f'{no_data_hspf}'
+            html += f'</td></tr>\n'
+        else:
+            if match:
+                match_symbol = f'<span style="font-weight:bold;color:red">X</span>'
+            else:
+                match_symbol = f'<span style="font-weight:bold;color:green">&#10003;</span>'
+            html = f'<tr><td>-</td><td>{con}</td><td>{diff}</td><td>{match_symbol}</td><td></td></tr>\n'
+        return html 
+
+    def _run_test(self) -> str:
+        futures = {}
+        results_dict = {}
+
+        #with ThreadPoolExecutor(max_workers=os.cpu_count()-1) as executor:
+        with ThreadPoolExecutor(max_workers=1) as executor:
+            for key in self.hspf_data.output_dictionary.keys():
+                (operation, activity, opn_id, tcode) = key.split('_')
+                if int(tcode) not in self.compare_tcodes or int(opn_id) not in self.compare_ids:
+                    continue 
+                for cons in self.hspf_data.output_dictionary[key]:
+                    params = (operation,activity,opn_id,cons,tcode)
+                    futures[executor.submit(self.check_con, params)] = params 
+            
+            for future in as_completed(futures):
+                key = futures[future]
+                results_dict[key] = future.result()
+
+        html = self.make_html_report(results_dict) 
+        return html
+
+    def check_con(self, params:Tuple[str,str,str,str,str]) -> Tuple[bool,bool,bool,float]:
+        """Performs comparision of single constituent"""
+        operation, activity, id, constituent, tcode = params
+        print(f'    {operation}_{id}  {activity}  {constituent}\n')
+        
+        ts_hsp2 = self.hsp2_data.get_time_series(operation, id, constituent, activity)
+        ts_hspf = self.hspf_data.get_time_series(operation, int(id), constituent, activity, 'hourly')
+
+        no_data_hsp2 = ts_hsp2 is None
+        no_data_hspf = ts_hspf is None
+        
+        if no_data_hsp2 or no_data_hspf:
+            return (no_data_hsp2, no_data_hspf, False, 0)
+        else:
+            #Special case, for some parameters (e.g PLANK.BENAL1) HSPF results look to be array.
+            #Working assumption is that only the first index of that array are the values of interest.
+            if len(ts_hspf.shape) > 1:
+                ts_hspf = ts_hspf.iloc[:,0]
+           
+            tolerance = 1e-2
+            # if heat related term, compute special tolerance
+            if constituent == 'IHEAT' or constituent == 'ROHEAT' or constituent.startswith('OHEAT') or \
+                constituent == 'QSOLAR' or constituent == 'QLONGW' or constituent == 'QEVAP' or \
+                constituent == 'QCON' or constituent == 'QPREC' or constituent == 'QBED':
+                abstol = max(abs(ts_hsp2.values.min()), abs(ts_hsp2.values.max())) * 1e-4
+            elif constituent == 'QTOTAL' or constituent == 'HTEXCH' :
+                abstol = max(abs(ts_hsp2.values.min()), abs(ts_hsp2.values.max())) * 1e-3    
+
+            ts_hsp2, ts_hspf = self.validate_time_series(ts_hsp2, ts_hspf,
+                self.hsp2_data, self.hspf_data, operation, activity, id, constituent)
+                
+            match, diff = self.compare_time_series(ts_hsp2, ts_hspf, tolerance)
+        
+        return (no_data_hsp2, no_data_hspf, match, diff)
 
     def fill_nan_and_null(self, timeseries:pd.Series, replacement_value:float = 0.0) -> pd.Series:
         """Replaces any nan or HSPF nulls -1.0e26 with provided replacement_value"""
@@ -171,20 +185,14 @@ class RegressTest(object):
 
         return ts_hsp2, ts_hspf
        
-    def compare_time_series(self, h5_time_series:pd.Series, hbn_time_series:pd.Series, 
-            html: str, tolerance:float, cons:str) -> str:
+    def compare_time_series(self, ts_hsp2:pd.Series, ts_hspf:pd.Series, tol:float) -> Tuple[bool, float]:
         
-        max_diff1 = (hbn_time_series.values - h5_time_series.values).max()
-        max_diff2 = (h5_time_series.values - hbn_time_series.values).max()
+        max_diff1 = (ts_hspf.values - ts_hsp2.values).max()
+        max_diff2 = (ts_hsp2.values - ts_hspf.values).max()
         max_diff = max(max_diff1, max_diff2)
         
-        if np.allclose(hbn_time_series, h5_time_series, rtol=1e-2, atol=tolerance, equal_nan=False):
-            match_symbol = f'<span style="font-weight:bold;color:red">X</span>'
-        else:
-            match_symbol = f'<span style="font-weight:bold;color:green">&#10003;</span>'
-
-        html += f'<tr><td>-</td><td>{cons}</td><td>{max_diff}</td><td>{match_symbol}</td><td></td></tr>\n'
-        return html
+        match = np.allclose(ts_hspf, ts_hsp2, rtol=1e-2, atol=tol, equal_nan=False)
+        return (match, max_diff)
 
 
     def run_test(self) -> None:
@@ -193,26 +201,23 @@ class RegressTest(object):
         source_root_path = os.path.split(os.path.split(current_directory)[0])[0]
         test_path = os.path.join(source_root_path, "tests")
 
+        test_dirs = os.listdir(test_path)
+        for test_dir in test_dirs:
+            if test_dir == self.compare_case:
+                test_path = os.path.join(test_path, test_dir)
+        
+        self._get_hdf5_data(test_path)
+        self._get_hbn_data(test_path)
+        #PRT - everything above this should to initializer 
+
         html_file = os.path.join(test_path, 'test_report_conversion.html')
         text_file = open(html_file, "w")
         text_file.write('<html><header><h1>CONVERSION TEST REPORT</h1></header><body>\n')
 
-        test_dirs = os.listdir(test_path)
-        test_dirs_selected = []
-        for test_dir in test_dirs:
-            if test_dir in self.compare_cases:
-                test_dirs_selected.append(test_dir)
-
-        test_case_count = 1
-        for test_dir in test_dirs_selected:
-            if test_dir not in self.compare_cases:
-                continue
-            else:
-                print(f'conversion test case: {test_dir} ({test_case_count} of {len(test_dirs_selected)})')
-                text_file.write(f'<h3>{os.path.join(test_path, test_dir)}</h3>\n')
-                one_test_report = self.run_one_test(os.path.join(test_path, test_dir))
-                text_file.write(one_test_report)
-            test_case_count += 1
+        print(f'conversion test case: {self.compare_case}')
+        text_file.write(f'<h3>{os.path.join(test_path, test_dir)}</h3>\n')
+        one_test_report = self._run_test()
+        text_file.write(one_test_report)
 
         text_file.write("</body></html>\n")
         text_file.close()
