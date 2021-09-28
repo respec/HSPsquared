bfa40db4e2fcf2ce05e6e4e4b7562cd3e7787c2e
Paul Tomasula <31142705+ptomasula@users.noreply.github.com>
2021-09-27 21:22:16 -0400

    performance overhaul
    Replaces regression_base run_one_test method with methods that single responsibility
    Introduces multithreading support

--- tests/convert/conversion_test.py
+++ tests/convert/conversion_test.py
@@ -1,15 +1,14 @@
 from datetime import datetime
 
-from tests.convert.regression_base import RegressTest
-
+from tests.convert.regression_base import RegressTest 
 if __name__ == '__main__':
     #compare_cases = ['test05', 'test09', 'test10', 'test10b', 'Calleg', 'GRW_Plaster', 'ZRW_WestIndian']
     #compare_cases = ['test10']  # control test cases for comparison
-    compare_cases = ['GLWACSO']  
+    compare_case = 'GLWACSO'  
 
     start = datetime.now()
 
-    my_test = RegressTest(compare_cases)
+    my_test = RegressTest(compare_case)
     my_test.run_test()
 
     print(f'runtime: {datetime.now() - start}')
