b963c3da253d70e445229425b2b0397e7acfdedd
Anthony Aufdenkampe <aaufdenkampe@limno.com>
2022-01-13 17:29:25 -0600

    Bump version to 0.10.0
    In preparation for the next release described with https://github.com/respec/HSPsquared/milestone/3

--- HSP2/__init__.py
+++ HSP2/__init__.py
@@ -7,6 +7,4 @@ from HSP2.main import main
 from HSP2.mainDoE import main as mainDoE
 from HSP2.utilities import versions, flowtype
 from _version import __version__
-# __version__ = '0.9.3'  # Added RQUAL modules (OXRX, NUTRX, PLANK, PHCARB)
-                       # Optimizations with Numba, bug fixes, metric units, more precisely matching HSPF results
-                       # Water Quality modules (0.9.0) plus new readWDM by block/group (0.9.1)
+
