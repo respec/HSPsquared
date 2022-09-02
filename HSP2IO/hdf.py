fa3071d43323c25fad61647bb4cd86d014ef455c
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2022-09-02 13:07:27 -0400

    try to add specactions property to h5 object

--- HSP2IO/hdf.py
+++ HSP2IO/hdf.py
@@ -72,6 +72,8 @@ class HDF5():
 					for i in range(int(start), int(stop)+1): uci.ddgener[module][f'G{i:03d}'] = row[2]
 			elif op == 'FTABLES':
 				uci.ftables[module] = self._store[path]
+			elif op == 'SPEC-ACTIONS':
+				uci.specactions[module] = self._store[path]
 			elif op == 'MONTHDATA':
 				if not uci.monthdata: uci.monthdata = {}
 				uci.monthdata[f'{op}/{module}'] = self._store[path]
