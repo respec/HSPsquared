e2c5e46daac36c7eabebb341441aa449b71cad0c
Burgholzer <robert.burgholzer@deq.virginia.gov>
2023-06-22 13:57:35 -0400

    debug

--- HSP2/state_fn_defaults.py
+++ HSP2/state_fn_defaults.py
@@ -4,6 +4,6 @@ from numba import int8, float32, njit, types, typed # import the types
 @njit
 def state_step_hydr(state_ix, dict_ix, ts_ix, hydr_ix, step):
     fn_defined = False
-    if step < 3:
+    if step <= 1:
         print("standard state_step_hydr() called")
     return
