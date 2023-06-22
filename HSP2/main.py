2c2ca533e57f57a17e5625ce52c644dd1cdfa9bd
Burgholzer <robert.burgholzer@deq.virginia.gov>
2023-06-22 13:47:17 -0400

    fix

--- HSP2/main.py
+++ HSP2/main.py
@@ -72,7 +72,7 @@ def main(io_manager:IOManager, saveall:bool=False, jupyterlab:bool=True) -> None
     # Now, load any OM components if present, and store variables on objects 
     hsp2_local_py = load_dynamics(io_manager, siminfo)
     # initialize state for hydr
-    hydr_init_ix(state_ix, state_paths, domain)
+    hydr_init_ix(state_ix, state_paths, state['domain'])
     print("siminfo", siminfo)
     # now put all of these Dicts into the state Dict 
     state['state_paths'], state['state_ix'], state['dict_ix'], state['ts_ix'] = state_paths, state_ix, dict_ix, ts_ix
