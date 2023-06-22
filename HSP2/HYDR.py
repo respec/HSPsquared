86100384cdc307ca3758f1501f31d16d1cfe5d5e
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2023-06-22 11:18:58 -0400

    pass state

--- HSP2/HYDR.py
+++ HSP2/HYDR.py
@@ -32,7 +32,7 @@ TOLERANCE = 0.001   # newton method max loops
 MAXLOOPS  = 100     # newton method exit tolerance
 
 
-def hydr(io_manager, siminfo, uci, ts, ftables, specactions):
+def hydr(io_manager, siminfo, uci, ts, ftables, state):
     ''' find the state of the reach/reservoir at the end of the time interval
     and the outflows during the interval
 
