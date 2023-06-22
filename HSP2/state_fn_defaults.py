d93d30eb66af3ce81ad7748d78685249a1d6f63a
Burgholzer <robert.burgholzer@deq.virginia.gov>
2023-06-22 12:42:19 -0400

    get the lib

--- HSP2/state_fn_defaults.py
+++ HSP2/state_fn_defaults.py
@@ -1,4 +1,6 @@
 # null function to be loaded when not supplied by user
+from numba import int8, float32, njit, types, typed # import the types
+
 @njit
 def state_step_hydr(state_ix, dict_ix, ts_ix, hydr_ix, step):
     fn_defined = False
