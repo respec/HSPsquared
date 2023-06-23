abe274dacfa6ee3d60d8f34ed6ff7e4a7823208c
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2023-06-23 12:12:18 -0400

    re enable all but dynamic module load

--- HSP2/HYDR.py
+++ HSP2/HYDR.py
@@ -127,25 +127,25 @@ def hydr(io_manager, siminfo, uci, ts, ftables, state):
         Olabels.append(f'O{i+1}')
         OVOLlabels.append(f'OVOL{i+1}')
     
-    # disabled for testing
-    '''
     # must split dicts out of state Dict since numba cannot handle mixed-type nested Dicts
     # state_info is some generic things about the simulation 
     state_info = Dict.empty(key_type=types.unicode_type, value_type=types.unicode_type)
     state_info['operation'], state_info['segment'], state_info['function'] = state['operation'], state['segment'], state['function']
     state_info['domain'], state_info['state_step_hydr'] = state['domain'], state['state_step_hydr']
     hsp2_local_py = state['hsp2_local_py']
+    # disabled for testing
+    '''
     # It appears necessary to load this here, instead of from main.py, otherwise,
     # _hydr_() does not recognize the function state_step_hydr()? 
     if (hsp2_local_py != False):
         from hsp2_local_py import state_step_hydr
     else:
         from HSP2.state_fn_defaults import state_step_hydr
+    '''
     state_ix, dict_ix, ts_ix = state['state_ix'], state['dict_ix'], state['ts_ix']
     state_paths = state['state_paths']
     # initialize the hydr paths in case they don't already reside here
     hydr_init_ix(state_ix, state_paths, state['domain'])
-    '''
     
     ###########################################################################
     # specactions - special actions code TBD
