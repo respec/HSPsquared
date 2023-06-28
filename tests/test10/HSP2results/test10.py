a01c44e18744f38cc400f3a99789aab5fbc87762
Robert W. Burgholzer <rburghol@vt.edu>
2023-06-28 18:04:37 +0000

    check for single rchres and activity in dynamic python test

--- tests/test10/HSP2results/test10.py
+++ tests/test10/HSP2results/test10.py
@@ -6,10 +6,11 @@ print("Loaded a set of  HSP2 code!")
 @njit
 def state_step_hydr(state_info, state_paths, state_ix, dict_ix, ts_ix, hydr_ix, step):
     if (step <= 1):
-        print("Custom state_step_hydr() called")
+        print("Custom state_step_hydr() called for ", state_info['segment'])
+        print("operation", state_info['operation'])
         print("state at start", state_ix)
         print("domain info", state_info)
     
-    if (state_info['segment'] == 'R001') and (state_info['operation'] == 'HYDR'):
+    if (state_info['segment'] == 'R001') and (state_info['activity'] == 'HYDR'):
         state_ix[hydr_ix['O1']] = 10.0 * 1.547
     return
