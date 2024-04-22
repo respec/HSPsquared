7bcae32b84afee80325e576b600baf263e2fceae
Tim Cera <tim@cerazone.net>
2024-04-22 19:14:50 -0400

    (feat) change to a pyproject.toml to publish to pypi
    * Moved metadata from setup.py to pyproject.toml and
      removed setup.py
    * Moved README.md to README.rst and rewrote.
    * Made a VERSION file to contain version number.
      + Allows version number to be read by pyproject.toml.
      + Refactored _version.py to read version number from
        VERSION file.
    * Made a publish.py to ease and document publishing to
      pypi.

---
+++ publish.py
@@ -0,0 +1,39 @@
+"""
+Publish package to PyPI.
+
+This script is used to publish the package to PyPI. It is a simple script that
+uses the `build` module to create the source distribution and wheel, and then
+uses `twine` to upload the package to PyPI.
+
+Need to install the following packages:
+- build (if using PyPI), python-build (if using conda-forge)
+- twine
+"""
+
+import shlex
+import shutil
+import subprocess
+
+PKG_NAME = "hsp2"
+
+with open("VERSION", encoding="ascii") as version_file:
+    version = version_file.readline().strip()
+
+shutil.rmtree("build", ignore_errors=True)
+
+subprocess.run(shlex.split("python3 -m build --sdist"), check=True)
+subprocess.run(shlex.split("python3 -m build --wheel"), check=True)
+sdist = f"dist/{PKG_NAME}-{version}.tar.gz"
+wheel = f"dist/{PKG_NAME}-{version}*.whl"
+
+for file in [sdist, wheel]:
+    subprocess.run(
+        shlex.split(f"twine check {file}"),
+        check=True,
+    )
+    subprocess.run(
+        shlex.split(f"twine upload --skip-existing {file}"),
+        check=True,
+    )
+
+shutil.rmtree("build", ignore_errors=True)
