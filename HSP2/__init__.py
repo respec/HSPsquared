18e663c3a1cf9ce055ab45328558a6fe82d5ba73
Austin Orr <aorr@geosyntec.com>
2024-04-29 14:02:14 -0700

    working install, clean packaging, working test case

--- HSP2/__init__.py
+++ HSP2/__init__.py
@@ -6,5 +6,4 @@ License: LGPL2
 from HSP2.main import main
 from HSP2.mainDoE import main as mainDoE
 from HSP2.utilities import versions, flowtype
-from _version import __version__
-
+from HSP2._version import __version__
--- _version.py
+++
@@ -1,4 +0,0 @@
-import os
-
-with open(os.path.join(os.path.dirname(__file__), "VERSION"), encoding="ascii") as version_file:
-    __version__ = version_file.read().strip()
