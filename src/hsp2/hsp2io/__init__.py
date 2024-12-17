9c1608c237846391410b2087186e03d52c82186c
Anthony Aufdenkampe <aaufdenkampe@limno.com>
2024-12-17 10:55:57 -0600

    Fixed version attribute for all modules for develop mode; Add module docstrings
    @timcera, this commit fixes the version attribute import error when in conda develop mode, while also preserving the versioning for packaging. It does so by moving the code from `_version.py` into the main init file. Thanks @ptomasula for helping me work through this "cake-and-eat-it-too" approach!

---
+++ src/hsp2/hsp2io/__init__.py
@@ -0,0 +1,6 @@
+"""The `hsp2io` module contains an abstracted approach to getting data in and 
+    out of HSP2 for flexibility and performance and also to support future 
+    automation and model coupling.
+"""
+
+from hsp2 import __version__
