9736956715c0abe43bb8ba5dd135c9cbd96de777
Rob <rburghol@vt.edu>
2025-10-16 17:33:52 +0000

    applied formatting changes

--- tests/testcbp/HSP2results/check_equation.py
+++ tests/testcbp/HSP2results/check_equation.py
@@ -7,6 +7,7 @@ from hsp2.hsp2.main import *
 from hsp2.hsp2.om import *
 from hsp2.hsp2io.hdf import HDF5
 from hsp2.hsp2io.io import IOManager
+from hsp2.state.state import *
 
 fpath = "./tests/testcbp/HSP2results/JL1_6562_6560.h5"
 # try also:
