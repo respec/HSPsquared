c4ac725e412b5439a910ecd0574e4213bd756d46
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2025-10-21 15:03:00 -0400

    update timer state settings

--- src/hsp2/hsp2/om_sim_timer.py
+++ src/hsp2/hsp2/om_sim_timer.py
@@ -28,80 +28,23 @@ class SimTimer(ModelObject):
 
     def register_components(self):
         # initialize the path variable if not already set
-        self.ix = set_state(
-            self.state.state_ix,
-            self.state.state_paths,
+        self.ix = self.state.set_state(
             self.state_path,
-            float(self.time_array[0][0]),
+            float(self.time_array[0][0])
         )
         # now register all other paths.
         # register "year", "month" "day", ...
-        year_ix = set_state(
-            self.state.state_ix,
-            self.state.state_paths,
-            "/STATE/year",
-            float(self.time_array[0][1]),
-        )
-        month_ix = set_state(
-            self.state.state_ix,
-            self.state.state_paths,
-            "/STATE/month",
-            float(self.time_array[0][2]),
-        )
-        day_ix = set_state(
-            self.state.state_ix,
-            self.state.state_paths,
-            "/STATE/day",
-            float(self.time_array[0][3]),
-        )
-        hr_ix = set_state(
-            self.state.state_ix,
-            self.state.state_paths,
-            "/STATE/hour",
-            float(self.time_array[0][4]),
-        )
-        min_ix = set_state(
-            self.state.state_ix,
-            self.state.state_paths,
-            "/STATE/minute",
-            float(self.time_array[0][5]),
-        )
-        sec_ix = set_state(
-            self.state.state_ix,
-            self.state.state_paths,
-            "/STATE/second",
-            float(self.time_array[0][6]),
-        )
-        wd_ix = set_state(
-            self.state.state_ix,
-            self.state.state_paths,
-            "/STATE/weekday",
-            float(self.time_array[0][7]),
-        )
-        dt_ix = set_state(
-            self.state.state_ix,
-            self.state.state_paths,
-            "/STATE/dt",
-            float(self.time_array[0][8]),
-        )
-        jd_ix = set_state(
-            self.state.state_ix,
-            self.state.state_paths,
-            "/STATE/jday",
-            float(self.time_array[0][9]),
-        )
-        md_ix = set_state(
-            self.state.state_ix,
-            self.state.state_paths,
-            "/STATE/modays",
-            float(self.time_array[0][10]),
-        )
-        dts_ix = set_state(
-            self.state.state_ix,
-            self.state.state_paths,
-            "/STATE/dts",
-            float(self.time_array[0][8] * 60.0),
-        )
+        year_ix = self.state.set_state("/STATE/year", float(self.time_array[0][1]) )
+        month_ix = self.state.set_state("/STATE/month", float(self.time_array[0][2]))
+        day_ix = self.state.set_state( "/STATE/day", float(self.time_array[0][3]))
+        hr_ix = self.state.set_state( "/STATE/hour", float(self.time_array[0][4]))
+        min_ix = self.state.set_state( "/STATE/minute", float(self.time_array[0][5]))
+        sec_ix = self.state.set_state( "/STATE/second",  float(self.time_array[0][6]))
+        wd_ix = self.state.set_state( "/STATE/weekday", float(self.time_array[0][7]))
+        dt_ix = self.state.set_state( "/STATE/dt", float(self.time_array[0][8]))
+        jd_ix = self.state.set_state( "/STATE/jday", float(self.time_array[0][9]))
+        md_ix = self.state.set_state( "/STATE/modays", float(self.time_array[0][10]))
+        dts_ix = self.state.set_state( "/STATE/dts", float(self.time_array[0][8] * 60.0))
         self.date_path_ix = [
             year_ix,
             month_ix,
