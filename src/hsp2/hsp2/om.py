4fbfe8cc78db9c17925cec478743eb2b7476644d
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2025-10-21 14:37:38 -0400

    Need tass cache to model root object creation

--- src/hsp2/hsp2/om.py
+++ src/hsp2/hsp2/om.py
@@ -168,7 +168,7 @@ def state_om_model_root_object(state, om_operations, siminfo):
     # Create the base that everything is added to. this object does nothing except host the rest.
     if "model_root_object" not in om_operations.keys():
         model_root_object = ModelObject(
-            state.model_root_name, False, {}, state
+            state.model_root_name, False, {}, state, om_operations['model_object_cache']
         )  # we give this no name so that it does not interfer with child paths like timer, year, etc (i.e. /STATE/year, ...)
         om_operations["model_root_object"] = model_root_object
         # set up the timer as the first element
