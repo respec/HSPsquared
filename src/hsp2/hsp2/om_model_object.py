25729bd6124d778caa87e45f431d26c28a93df4a
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2025-11-10 12:14:18 -0500

    oops use object notation

--- src/hsp2/hsp2/om_model_object.py
+++ src/hsp2/hsp2/om_model_object.py
@@ -287,8 +287,8 @@ class ModelObject:
         if not (self.container == False):
             return self.container.find_var_path(var_name)
         # check for model_root_object state vars model_root_object + var_name
-        if (state.model_root_object + "/" + var_name) in self.state.state_paths:
-            return state.model_root_object + "/" + var_name
+        if (self.state.model_root_object + "/" + var_name) in self.state.state_paths:
+            return self.state.model_root_object + "/" + var_name
         # check for root state vars STATE + var_name
         if ("/STATE/" + var_name) in self.state.state_paths:
             return "/STATE/" + var_name
