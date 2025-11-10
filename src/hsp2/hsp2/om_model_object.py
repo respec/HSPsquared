d0a2a3cc551bd83f2d988439a97531b6bde8b556
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2025-11-10 12:12:32 -0500

    use model_root_object as state base

--- src/hsp2/hsp2/om_model_object.py
+++ src/hsp2/hsp2/om_model_object.py
@@ -287,9 +287,9 @@ class ModelObject:
         if not (self.container == False):
             return self.container.find_var_path(var_name)
         # check for root state vars STATE + var_name
-        if ("/STATE/" + var_name) in self.state.state_paths:
+        if (state.model_root_object + "/" + var_name) in self.state.state_paths:
             # return self.state['state_paths'][("/STATE/" + var_name)]
-            return "/STATE/" + var_name
+            return state.model_root_object + "/" + var_name
         # check for full paths
         if var_name in self.state.state_paths:
             # return self.state['state_paths'][var_name]
