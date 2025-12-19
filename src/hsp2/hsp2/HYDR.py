37c5b0faba6f1e817d73169a8b54367b716cc31d
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2025-12-19 10:11:44 -0500

    debug

--- src/hsp2/hsp2/HYDR.py
+++ src/hsp2/hsp2/HYDR.py
@@ -418,7 +418,7 @@ def _hydr_(
             # OUTDGT is writeable
             for oi in range(nexits):
                 if step <= 5:
-                    print("Getting O var", oi, "with state index", out_ix[oi], "and path", state.get_ix_path(out_ix[oi]), "from state ix", out_ix[oi],"val=", state.state_ix[out_ix[oi]]
+                    print("Getting O var", oi, "with state index", out_ix[oi], "and path", state.get_ix_path(out_ix[oi]), "from state ix", out_ix[oi],"val=", state.state_ix[out_ix[oi]])
                 outdgt[oi] = state.state_ix[out_ix[oi]]
             # IVOL is writeable.
             # Note: we must convert IVOL to the units expected in _hydr_
