07938cf760dac4beb8a9d33ff7b31c9e9ae9ac16
Robert W. Burgholzer <rburghol@vt.edu>
2026-01-02 21:14:07 +0000

    Add update_uci mode in CLI and test UCI files

--- src/hsp2/hsp2tools/commands.py
+++ src/hsp2/hsp2tools/commands.py
@@ -57,3 +57,30 @@ def import_uci(ucifile, h5file):
             wdmfile = (uci_dir / nline[16:].strip()).resolve()
             if wdmfile.exists():
                 readWDM(wdmfile, h5file)
+
+
+
+
+def update_uci(ucifile, h5file):
+    """Import UCI only into HDF5 file.
+
+    Parameters
+    ----------
+    ucifile: str
+        The UCI file to import into HDF file.
+    h5file: str
+        The destination HDF5 file.
+    """
+    # we send False here to prevent deleting and recreating the UCI
+    print("Updating parameters in h5 file from UCI.", ucifile)
+    print("Note: this will NOT update external data such as WDM, mutsin etc.")
+    print("To update all data, use the command 'hsp2 import_uci ...' ")
+    readUCI(ucifile, h5file, False)
+    with open(ucifile) as fp:
+        uci = []
+        for line in fp.readlines():
+            if "***" in line[:81]:
+                continue
+            if not line[:81].strip():
+                continue
+            uci.append(line[:81].rstrip())
