d9001b7d8d457a0addba167d40cc8e311ef21296
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2026-01-06 13:36:38 -0500

    no complex setting in init

--- src/hsp2/state/state.py
+++ src/hsp2/state/state.py
@@ -63,7 +63,7 @@ state_lite = [
 class state_class_lite:
     def __init__(self):
         self.num_ops = 0
-        state_ix = zeros(self.num_ops)
+        #state_ix = zeros(self.num_ops)
         #self.state_ix = state_ix.astype(npfloat64)
         #op_tokens = zeros((self.num_ops, 64))
         #self.op_tokens = op_tokens.astype(npint64)
