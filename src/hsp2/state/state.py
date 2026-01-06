61ac91340a61c3b40ef94988a61f63b2cd71e203
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2026-01-06 13:25:26 -0500

    simpler spec

--- src/hsp2/state/state.py
+++ src/hsp2/state/state.py
@@ -63,14 +63,14 @@ class state_class_lite:
         self.num_ops = 0
         state_ix = zeros(self.num_ops)
         self.state_ix = state_ix.astype(npfloat64)
-        op_tokens = zeros((self.num_ops, 64))
-        self.op_tokens = op_tokens.astype(npint64)
+        #op_tokens = zeros((self.num_ops, 64))
+        #self.op_tokens = op_tokens.astype(npint64)
         # TODO: move to individual objects in OM/RCHRES/PERLND/...
-        op_exec_lists = zeros((self.num_ops, 1024))
-        self.op_exec_lists = op_exec_lists.astype(npint64)
+        #op_exec_lists = zeros((self.num_ops, 1024))
+        #self.op_exec_lists = op_exec_lists.astype(npint64)
         # TODO: is this even needed? Since each domain has it's own exec list?
         model_exec_list = zeros(self.num_ops)
-        self.model_exec_list = model_exec_list.astype(npint64)
+        #self.model_exec_list = model_exec_list.astype(npint64)
 
 @njit(cache=True)
 def make_state_lite():
