17dde1d1bb3b805eaa7bf9d8735dd72a4b94d8ef
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2026-01-06 12:13:37 -0500

    remove unused import

--- src/hsp2/hsp2/om_sim_timer.py
+++ src/hsp2/hsp2/om_sim_timer.py
@@ -4,7 +4,6 @@ It is also used to make an implicit parent child link to insure that an object i
 during a model simulation.
 """
 
-from hsp2.state.state import state
 from hsp2.hsp2.om import ModelObject
 from hsp2.hsp2.om_model_object import ModelObject
 from pandas import DataFrame
