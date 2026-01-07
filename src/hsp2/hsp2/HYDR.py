a92d57ddf08bc84649784385fd516dce89235aa9
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2026-01-07 15:20:21 -0500

    debug

--- src/hsp2/hsp2/HYDR.py
+++ src/hsp2/hsp2/HYDR.py
@@ -394,10 +394,12 @@ def _hydr_(
         state.state_ix[volev_ix] = volev
         # - these if statements may be irrelevant if default functions simply return
         #   when no objects are defined.
-        if step < 3:
-            print("Calling pre_step_model")
         if state.state_step_om == "enabled":
+            if step < 3:
+                print("Calling pre_step_model")
             pre_step_model(model_exec_list, state.op_tokens, state.state_ix, state.dict_ix, state.ts_ix, step)
+            if step < 3:
+                print("Calling pre_step_model")
         if step < 3:
             print("Calling state_step_hydr")
         if state.state_step_hydr == "enabled":
