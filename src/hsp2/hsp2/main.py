c4d976b80108063eefa55e655872b094b84dfe1b
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2026-01-07 14:24:26 -0500

    fix args

--- src/hsp2/hsp2/main.py
+++ src/hsp2/hsp2/main.py
@@ -104,18 +104,24 @@ def main(
     # Add support for dynamic functions to operate on STATE
     # - Load any dynamic components if present, and store variables on objects
     state_load_dynamics_hsp2(state, io_manager, siminfo)
+    print("state_load_dynamics_hsp2()", timer.split(), "seconds")
     # Iterate through all segments and add crucial paths to state
     # before loading dynamic components that may reference them
-    state_init_hsp2(state, opseq, activities, om_operations)
+    state_init_hsp2(state, opseq, activities)
+    print("state_init_hsp2()", timer.split(), "seconds")
     # now initialize all state variables for mutable variables
     hsp2_domain_dependencies(state, opseq, activities, om_operations, False)
+    print("hsp2_domain_dependencies()", timer.split(), "seconds")
     # - finally stash specactions in state, not domain (segment) dependent so do it once
     specl_load_om(om_operations, specactions)  # load traditional special actions
+    print("specl_load_om()", timer.split(), "seconds")
     state_load_dynamics_om(
         state, io_manager, siminfo, om_operations
     )  # operational model for custom python
+    print("state_load_dynamics_om()", timer.split(), "seconds")
     # finalize all dynamically loaded components and prepare to run the model
     state_om_model_run_prep(opseq, activities, state, om_operations, siminfo)
+    print("state_om_model_run_prep()", timer.split(), "seconds")
     #######################################################################################
 
     # main processing loop
