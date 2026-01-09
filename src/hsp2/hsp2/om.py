82d4a39cdabdb5fdfb04838755b161cf6fb6bd82
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2026-01-09 10:50:34 -0500

    debug

--- src/hsp2/hsp2/om.py
+++ src/hsp2/hsp2/om.py
@@ -726,10 +726,10 @@ def pre_step_model(model_exec_list, op_tokens, state_ix, dict_ix, ts_ix, step):
 
 @njit
 def step_model(model_exec_list, op_tokens, state_ix, dict_ix, ts_ix, step):
-    m = 0
+    n = 0
     for i in model_exec_list:
         if n < 20 and step < 2:
-            print op_tokens[i]
+            print(op_tokens[i])
         step_one(op_tokens, op_tokens[i], state_ix, dict_ix, ts_ix, step, 0)
         n = n + 1
     return
