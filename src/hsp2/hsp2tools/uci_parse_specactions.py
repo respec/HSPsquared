e1207bb49db33e2bef2aa701aa7f28f0d55b48d3
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2026-01-20 12:44:56 -0500

    fdebug

--- src/hsp2/hsp2tools/uci_parse_specactions.py
+++ src/hsp2/hsp2tools/uci_parse_specactions.py
@@ -107,6 +107,8 @@ def specl_get_parent_condition(open_conditions):
 
 def get_ifs(lines, line, line_end='THEN'):
     end_ln = len(line_end)
+    print("Start line:", line)
+    print("********************")
     while line[-end_ln:] != line_end:
         nline = next(lines).strip()
         print(line, nline)
