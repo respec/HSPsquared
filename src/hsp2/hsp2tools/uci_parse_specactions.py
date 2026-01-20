101e1e5623303faca53dd89cef069f4a30240fd5
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2026-01-20 16:05:39 -0500

    update name in dataframe for condition proper maping

--- src/hsp2/hsp2tools/uci_parse_specactions.py
+++ src/hsp2/hsp2tools/uci_parse_specactions.py
@@ -63,7 +63,7 @@ def specactions_parse(info, llines):
             d = ucifn.parseD(line, parse["SPEC-ACTIONS", "conditions"])
             sibling_id =  sa_conditions[-1,]['cond_id']
             d['sibling_id'] = sibling_id
-            d["parent_id"] = parent_condition
+            d["parent_id"] = specl_get_parent_condition(open_conditions)
         elif line.strip() == "END IF":
             #print("found END IF")
             # must replace this with a stack to push/pop
@@ -75,7 +75,7 @@ def specactions_parse(info, llines):
             # todo: Tim has a single function that parses a line
             print("trYING ACtion PARSER", line)
             d = ucifn.parseD(line, parse["SPEC-ACTIONS", "ACTIONS"])
-            d["parent_id"] = specl_get_parent_condition(open_conditions)
+            d["condition"] = specl_get_parent_condition(open_conditions)
             sa_actions.append(d)
     
     if sa_actions:
