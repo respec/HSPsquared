53abfa67d9b6edd7f1b81225d4c6a8b0c8134cdb
Robert W. Burgholzer <rburghol@vt.edu>
2026-02-03 21:09:58 +0000

    wait till pandas 3 to update tests

--- tests/convert/regression_base.py
+++ tests/convert/regression_base.py
@@ -8,8 +8,7 @@ from typing import Dict, List, Tuple, Union
 import numpy as np
 import pandas as pd
 from hsp2.hsp2tools.HBNOutput import HBNOutput
-from hsp2.hsp2tools.HDF5 import HDF5, hsp2_hspf_aliases
-import hsp2.hsp2tools as hsp2tools
+from hsp2.hsp2tools.HDF5 import HDF5
 
 OperationsTuple = Tuple[str, str, str, str, str]
 ResultsTuple = Tuple[bool, bool, bool, float]
@@ -32,7 +31,6 @@ class RegressTest:
         self.ids = ids
         self.threads = threads
         self.quiet = False # allows users to set this later
-        self.aliases = hsp2_hspf_aliases() # compatibility map btwn hspf:hsp2
         self._init_files()
 
     def _init_files(self):
@@ -343,10 +341,3 @@ class RegressTest:
 
         match = np.allclose(ts_hspf, ts_hsp2, rtol=1e-2, atol=tol, equal_nan=False)
         return (match, max_diff)
-    
-    def _read_aliases_csv(self) -> Dict[Tuple[str, str, str], str]:
-        datapath = os.path.join(hsp2tools.__path__[0], "data", "HBNAliases.csv")
-        df = pd.read_csv(datapath)
-        df = df.set_index(["operation", "activity", "hspf_name"])
-        df_dict = df["hsp2_name"].to_dict()
-        return df_dict
