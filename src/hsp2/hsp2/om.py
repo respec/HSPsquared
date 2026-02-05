99eebbc7fbabd36e2aa3de7ded4a3efff0811599
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2026-02-04 21:42:13 -0500

    renamed the timer import to the new one

--- src/hsp2/hsp2/om.py
+++ src/hsp2/hsp2/om.py
@@ -11,7 +11,7 @@ import numpy as np
 from pandas import Series, DataFrame
 from numba import njit  # import the types
 from numpy import zeros
-from hsp2.hsp2.om_timer import timer_class
+from hsp2.state.state_timer import timer_class
 from hsp2.hsp2.SPECL import specl_load_om
 from hsp2.state.state import (
     append_state,
