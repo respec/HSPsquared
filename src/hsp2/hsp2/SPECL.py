f49d62f9d0d2c010935b39388e66013001d18af4
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2026-02-05 20:22:02 -0500

    use valid string

--- src/hsp2/hsp2/SPECL.py
+++ src/hsp2/hsp2/SPECL.py
@@ -23,7 +23,7 @@ def specl_load_om(state, parameter_obj):
             id = hsp2_sequence_id(speca['OPTYP'].iloc[0], speca['RANGE1'].iloc[0])
             if id not in parameter_obj.opseq:
                 warnings.warn(
-                    "Missing/inactive segment" + speca['OPTYP'] + ' ' + str(speca['RANGE1']), "referenced.",
+                    "Missing/inactive segment" + id + "referenced.",
                     DeprecationWarning)
                 continue
             state["model_data"][opname] = {}
